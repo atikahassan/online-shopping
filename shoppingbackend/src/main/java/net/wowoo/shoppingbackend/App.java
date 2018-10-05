39652c5c99a4ff295feeb1730bc5e373242eb40d
Atika Hassan <atikahassan.ah@yahoo.com>
2018-10-05 10:34:26 +0800

    My Project Setup Completed Successfully

---
+++ shoppingbackend/src/main/java/net/wowoo/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.wowoo.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
